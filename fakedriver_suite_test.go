ab14ac6cec3eca1b200e908a45be1e8a529db611
Julian Hjortshoj <julian.hjortshoj@emc.com>
2016-06-14 11:48:22 -0700

    move fakedriver into localdriver
    Signed-off-by: Connor Braa <cbraa@pivotal.io>

---
+++ fakedriver_suite_test.go
@@ -0,0 +1,13 @@
+package localdriver_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestFakedriver(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "FakeDriver Suite")
+}
