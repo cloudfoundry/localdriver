9c3f702c740b56bd718903df0b666e966575be4d
Paul Warren <paul.warren@emc.com>
2018-01-31 10:27:56 -0800

    fix oshelper windows implementation
    [#154075645](https://www.pivotaltracker.com/story/show/#154075645)
    
    Signed-off-by: Kai Xiang <kxiang@pivotal.io>

--- oshelper/oshelper_windows.go
+++ oshelper/oshelper_windows.go
@@ -2,12 +2,15 @@
 
 package oshelper
 
+import "code.cloudfoundry.org/localdriver"
+
 type osHelper struct {
 }
 
-func NewOsHelper() OsHelper {
+func NewOsHelper() localdriver.OsHelper {
 	return &osHelper{}
 }
 
 func (o *osHelper) Umask(mask int) (oldmask int) {
+	return 0
 }
